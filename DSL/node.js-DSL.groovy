8a10db7617d3df32fb3cedc14aad43dafd8d1e88
macloujulian <65403593+macloujulian@users.noreply.github.com>
2020-10-21 11:13:39 -0300

    Update node.js-DSL.groovy

--- DSL/node.js-DSL.groovy
+++ DSL/node.js-DSL.groovy
@@ -1,1 +1,18 @@
-y
+job('Aplicacion Node.js DSL') {
+    description('Aplicación Node JS DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('Usuario DSL')
+            node / gitConfigEmail('macloujulian@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('???¿¿¿¿¿¿¿¿¿')
+    }
+    steps {
+        shell("npm install")
+    }
+}
