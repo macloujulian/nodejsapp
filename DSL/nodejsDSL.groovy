9a69172781a63112ad0fc2239918e7a05070c2f9
michelvatre <140221258+michelvatre@users.noreply.github.com>
2023-08-08 19:55:43 -0600

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/macloujulian/nodejsapp.git', 'master-1') { node ->
+            node / gitConfigName('michelvatre')
+            node / gitConfigEmail('michelvatre94@gmail.com')
         }
     }
     triggers {
